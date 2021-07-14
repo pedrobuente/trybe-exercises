5336b8000da2c2b07a5ff6738be0bbc1c96c9471
Pedro Silva Buente <pedrobuente@gmail.com>
2021-07-14 14:23:53 -0300

    commit inicial bloco 6

---
+++ modulo_fundamentos/bloco4_introducao_java/p4_scripts.js
@@ -0,0 +1,24 @@
+let trybe = 16.42;
+if(trybe >= 14 && trybe < 14.40) {
+    console.log("Esquenta");
+}
+else if(trybe >= 16.30 && trybe < 17.50) {
+    console.log("Aula ao vivo");
+}
+else if(trybe >= 19.40 && trybe < 20) {
+    console.log("Fechamento")
+}
+else {
+    console.log("Fora dos momentos síncronos");
+}
+
+var nota = 55;
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado!");
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na lista de espera!")
+}
+else {
+    console.log("Você foi reprovado(a)!")
+}
---
+++ modulo_fundamentos/bloco4_introducao_java/p5_scripts.js
@@ -0,0 +1,16 @@
+let estadoPessoas = "reprovad";
+
+switch (estadoPessoas){
+
+case "aprovada":
+    console.log("aprovada");
+    break;
+case "reprovada":
+    console.log("reprovada");
+    break;
+case "lista":
+    console.log("lista");
+    break;
+default:
+    console.log("não se aplica");
+}
